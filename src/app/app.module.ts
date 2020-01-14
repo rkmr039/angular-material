d5d7df7acef720c0c2a3ae3e4fbdfbdb06ed064a
rkmr039 <rkmr039@gmail.com>
2020-01-14 20:39:24 +0530

    saperate module is added for material imports and exports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material';
+import {MaterialModule} from './material/material.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +13,7 @@ import {MatButtonModule} from '@angular/material';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
